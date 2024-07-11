39988fb893f4ee02a940186326669164234ad120
gzk47 <98312059+gzk47@users.noreply.github.com>
2024-07-11 17:26:36 +0800

    Update

--- .github/workflows/switchScript_Pure.sh
+++ .github/workflows/switchScript_Pure.sh
@@ -515,8 +515,8 @@ else
     mv Firmware-Dumper.nro ./switch/Firmware-Dumper
 fi
 
-### Fetch lastest nxdumptool(nxdt_rw_poc) from https://github.com/DarkMatterCore/nxdumptool/releases/tag/rewrite-prerelease/nxdt_rw_poc.nro
-curl -sL https://github.com/DarkMatterCore/nxdumptool/releases/tag/rewrite-prerelease/nxdt_rw_poc.nro -o nxdt_rw_poc.nro
+### Fetch lastest nxdumptool(nxdt_rw_poc) from https://github.com/DarkMatterCore/nxdumptool/releases/download/rewrite-prerelease/nxdt_rw_poc.nro
+curl -sL https://github.com/DarkMatterCore/nxdumptool/releases/download/rewrite-prerelease/nxdt_rw_poc.nro -o nxdt_rw_poc.nro
 if [ $? -ne 0 ]; then
     echo "nxdt_rw_poc download\033[31m failed\033[0m."
 else

aa00eca9483c5aa900c03707692b7eeb9e492c23
gzk47 <98312059+gzk47@users.noreply.github.com>
2024-07-11 09:19:24 +0800

    Update

--- .github/workflows/switchScript_Pure.sh
+++ .github/workflows/switchScript_Pure.sh
@@ -838,7 +838,7 @@ if [ $? -ne 0 ]; then
     echo "readme download\033[31m failed\033[0m."
 else
     echo "readme download\033[32m success\033[0m."
-    mv readme.txt 【纯净版】心悦大气层中文整合包 v$(date +%Y%m%d).txt
+    mv readme.txt 【纯净版】心悦大气层中文整合包v$(date +%Y%m%d).txt
 
 fi
 

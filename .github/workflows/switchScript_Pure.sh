32738a2238bbb0cdef9b6fa27800c916c03fffa6
gzk47 <98312059+gzk47@users.noreply.github.com>
2024-07-21 14:17:40 +0800

    更新 switchScript_Pure.sh

--- .github/workflows/switchScript_Pure.sh
+++ .github/workflows/switchScript_Pure.sh
@@ -707,6 +707,11 @@ stock=1
 icon=bootloader/res/switch.bmp
 id=ofw-sys
 {机身正版系统}
+
+[CFW-AUTO]
+payload=bootloader/payloads/fusee.bin
+icon=bootloader/res/auto.bmp
+{大气层-自动识别}
 ENDOFFILE
 if [ $? -ne 0 ]; then
     echo "Writing hekate_ipl.ini in ./bootloader/ directory\033[31m failed\033[0m."
@@ -717,11 +722,6 @@ fi
 ### Write more.ini in /bootloader/ini/
 #mkdir -p ./SwitchSD-Pure/bootloader/ini
 cat > ./bootloader/ini/more.ini << ENDOFFILE
-[CFW-AUTO]
-payload=bootloader/payloads/fusee.bin
-icon=bootloader/res/auto.bmp
-{大气层-自动识别}
-
 [SXOS]
 payload=bootloader/payloads/sxos.bin
 icon=bootloader/res/sxos.bmp

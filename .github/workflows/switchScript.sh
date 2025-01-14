c2fb171b8393d27b65860317bd13a3e3ee401e4e
gzk47 <98312059+gzk47@users.noreply.github.com>
2025-01-14 16:12:17 +0800

    Update switchScript.sh

--- .github/workflows/switchScript.sh
+++ .github/workflows/switchScript.sh
@@ -91,21 +91,20 @@ fi
 #ENDOFFILE
 ###
 
-### Fetch sys-patch from https://github.com/impeeza/sys-patch/releases/latest
-curl -sL https://api.github.com/repos/impeeza/sys-patch/releases/latest \
-  | jq '.tag_name' \
-  | xargs -I {} echo sys-patch {} >> ../description.txt
-curl -sL https://api.github.com/repos/impeeza/sys-patch/releases/latest \
-  | grep -oP '"browser_download_url": "\Khttps://[^"]*Sys-Patch[^"]*.7z"' \
+### Fetch sys-patch from https://github.com/borntohonk/sys-patch/releases/latest
+curl -sL https://api.github.com/repos/borntohonk/sys-patch/releases/latest \
+  | jq '.name' \
+  | xargs -I {} echo {} >> ../description.txt
+curl -sL https://api.github.com/repos/borntohonk/sys-patch/releases/latest \
+  | grep -oP '"browser_download_url": "\Khttps://[^"]*sys-patch.zip"' \
   | sed 's/"//g' \
-  | xargs -I {} curl -sL {} -o Sys-Patch.7z
-  mv Sys-Patch.7z Sys-Patch.zip
+  | xargs -I {} curl -sL {} -o sys-patch.zip
 if [ $? -ne 0 ]; then
     echo "sys-patch download\033[31m failed\033[0m."
 else
     echo "sys-patch download\033[32m success\033[0m."
-    unzip -oq Sys-Patch.zip
-    rm Sys-Patch.zip
+    unzip -oq sys-patch.zip
+    rm sys-patch.zip
 fi
 
 ### Fetch lastest theme-patches from https://github.com/exelix11/theme-patches
